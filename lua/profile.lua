344b510bed3e5055793bf3714c92a10e4c283103
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2024-01-06 01:26:35 +0800

    添加 lazy 包管理和 nvim 自动安装

---
+++ lua/profile.lua
@@ -0,0 +1,8 @@
+vim.opt.fileencoding = "utf-8"  -- 文件编码
+vim.opt.number = true           -- 显示行号
+
+vim.opt.tabstop = 4             -- 按 1 次 tab 显示 4 个空格数量
+vim.opt.softtabstop = 4         -- 按 1 次删除清除空格数量
+vim.opt.shiftwidth = 4          -- 换行缩进 4 个空格
+vim.opt.autoindent = true       -- 自动缩进
+vim.opt.expandtab = true        -- 用空格替换 tab
