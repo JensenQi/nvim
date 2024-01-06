408f85f711b0ccab09bceb2a41699ba4581913c9
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2024-01-06 14:26:20 +0800

    添加 expand-region 代码块快速选择插件

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -7,4 +7,7 @@ map("n", "<Space>", "", opt) -- 关闭空格键的移动, 仅作为 Leader 键
 map("n", "gh", "0", opt)
 map("n", "ge", "$", opt)
 
+map("v", "v", "<Plug>(expand_region_expand)", opt)
+map("v", "V", "<Plug>(expand_region_shrink)", opt)
+
 
---
+++ lua/plugins/expand-region.lua
@@ -0,0 +1,6 @@
+return {
+    {
+        os.getenv("ghproxy") .. "https://github.com/terryma/vim-expand-region.git",
+        version = "*"
+    }
+}
