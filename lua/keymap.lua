57065b1a9c18772725999e5cbf1808ec74c0600d
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2024-01-08 19:54:53 +0800

    修改查找快捷键

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -13,8 +13,8 @@ map("v", "V", "<Plug>(expand_region_shrink)", opt) -- 缩小s选择块
 map("n", "<C-e>", "<C-w>w", opt) -- File Explorer 与 Editor 切换 
 
 map("n", "<A-f>", "/", opt) -- 页面内关键词查找
-map("n", "<A-Space>", ":Telescope live_grep<CR>", opt) -- 全局关键词查找
-map("n", "<C-f>", ":Telescope find_files<CR>", opt) -- 全局文件查找
+map("n", "<C-F>", ":Telescope live_grep<CR>", opt) -- 全局关键词查找
+map("n", "<C-o>", ":Telescope find_files<CR>", opt) -- 全局文件查找
 
-map("n", "<C-_>", '<Plug>(comment_toggle_linewise_current)j', opt) -- 快速注释当前行并移动到下一行
+map("n", "<C-_>", "<Plug>(comment_toggle_linewise_current)j", opt) -- 快速注释当前行并移动到下一行
 map("v", "<C-_>", "<Plug>(comment_toggle_linewise_visual)", opt) -- 快速注释选中块
