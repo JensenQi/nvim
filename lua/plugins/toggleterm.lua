ecb08339613eff2cc95dfe135795c4b8bfb7fa6e
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2024-01-08 21:31:23 +0800

    添加终端插件 toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        os.getenv("ghproxy") .. "https://github.com/akinsho/toggleterm.nvim.git",
+        version = "*",
+        config = function()
+            local buf_del = require("toggleterm")
+            require("toggleterm").setup({
+                open_mapping = [[<c-\>]],
+                start_in_insert = true,
+                direction = "horizontal",
+                autochdir = true
+            })
+        end
+    }
+}
