392a54c38b8417f93d19775ac2594ef84ce3d6a5
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2024-01-15 14:05:15 +0800

    添加括号自动补全

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,15 @@
+-- 自动补全括号插件
+return {
+    {
+        os.getenv("ghproxy") .. "https://github.com/windwp/nvim-autopairs.git",
+        version = "*",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup({
+                enable_afterquote = false, -- 避免 foo("bar" -> foo("bar")
+                map_cr = false -- 禁用 CR, 避免与自动补全的 CR 冲突
+            })
+        end
+    }
+}
+
