7f04b7176e2a7ddd1f8ea2f0634bf0eff5b5454a
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2024-05-14 10:42:55 +0800

    添加内容匹配部分高亮 & 跳转插件

---
+++ lua/plugins/matchup.lua
@@ -0,0 +1,13 @@
+--
+return {
+    {
+        os.getenv("ghproxy") .. "https://github.com/andymass/vim-matchup.git",
+        version = "*",
+        dependencies = {
+        },
+        config = function()
+            vim.g.matchup_matchparen_offscreen = { method = "popup" }
+        end
+    }
+}
+
